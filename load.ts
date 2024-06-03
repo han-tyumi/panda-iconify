7b6148451e63c8089becfe5f58fff9b4c6b93019
Matt Champagne <mmchamp95@gmail.com>
2024-06-03 14:55:16 -0400

    give control over definitions

--- load.ts
+++ load.ts
@@ -1,29 +1,33 @@
-import { IconifyJSONIconsData } from "./deps.ts";
+import { type IconifyJSONIconsData } from "./deps.ts";
 
 async function importJSON<T>(id: string): Promise<T> {
   return (await import(id, { with: { type: "json" } })).default;
 }
 
-export async function loadIconSet(name: string): Promise<IconifyJSONIconsData> {
+export async function loadIconSet(
+  prefix: string,
+): Promise<IconifyJSONIconsData> {
   const errors = [];
 
   try {
-    return await importJSON(`@iconify-json/${name}/icons.json`);
+    return await importJSON(`@iconify-json/${prefix}/icons.json`);
   } catch (error) {
     errors.push(error);
   }
 
   try {
-    return await importJSON(`@iconify/json/json/${name}.json`);
+    return await importJSON(`@iconify/json/json/${prefix}.json`);
   } catch (error) {
     errors.push(error);
   }
 
   throw new Error(
-    `Could not find '${name}' icon set\n` + errors.map(String).join("\n"),
+    `Could not find '${prefix}' icon set\n` + errors.map(String).join("\n"),
   );
 }
 
-export function loadIconSets(names: string[]): Promise<IconifyJSONIconsData[]> {
-  return Promise.all(names.map(loadIconSet));
+export function loadIconSets(
+  prefixes: string[],
+): Promise<IconifyJSONIconsData[]> {
+  return Promise.all(prefixes.map(loadIconSet));
 }
--- mod.ts
+++ mod.ts
@@ -10,39 +10,103 @@ import { getIconCSSRules } from "./get_icon_css_rules.ts";
 import { loadIconSets } from "./load.ts";
 
 export interface Options {
-  sets: string[];
+  /**
+   * The icon sets that you'd like to be defined as recipes.
+   *
+   * These must be made available through `@iconify/json`
+   * or individual icon set packages (e.g., `@iconify-json/mdi`).
+   */
+  prefixes: string[];
+
+  /**
+   * Controls which recipes are defined.
+   *
+   * @default
+   * ```js
+   * { icon: true }
+   * ```
+   */
+  define?: {
+    /**
+     * Defines an `icon` recipe where the `name` property takes a string
+     * containing the icon prefix and name separated by a `:`.
+     *
+     * @example
+     * ```js
+     * import { icon } from '~/styled-system/recipes'
+     * icon({ name: 'fa6-solid:address-book' })
+     * ```
+     */
+    icon?: boolean;
+
+    /**
+     * Defines a recipe for each prefix.
+     * The `name` property specifies the icon's name.
+     *
+     * @example
+     * ```js
+     * import { fa6Solid } from '~/styled-system/recipes'
+     * fa6Solid({ name: 'address-book' })
+     * ```
+     */
+    prefix?: boolean;
+
+    /**
+     * Defines a recipe for each icon using its prefix followed by its name.
+     *
+     * @example
+     * ```js
+     * import { fa6SolidAddressBook } from '~/styled-system/recipes'
+     * fa6SolidAddressBook()
+     * ```
+     */
+    prefixAndName?: boolean;
+  };
 }
 
-export default async function createPreset(options: Options): Promise<Preset> {
-  const iconSets = await loadIconSets(options.sets);
+export default async function createPreset(
+  { prefixes, define = { icon: true } }: Options,
+): Promise<Preset> {
+  const iconSets = await loadIconSets(prefixes);
 
-  const recipes: Record<string, Partial<RecipeConfig>> = {
-    icon: {
-      variants: {
-        name: {},
-      },
-    },
-  };
+  const recipes: Record<string, Partial<RecipeConfig>> = {};
 
-  for (const set of iconSets) {
-    const camelizedPrefix = camelize(set.prefix);
-
-    recipes[camelizedPrefix] = {
+  if (define.icon) {
+    recipes.icon = {
       variants: {
         name: {},
       },
     };
+  }
+
+  for (const set of iconSets) {
+    const camelizedPrefix = camelize(set.prefix);
+
+    if (define.prefix) {
+      recipes[camelizedPrefix] = {
+        variants: {
+          name: {},
+        },
+      };
+    }
 
     for (const [name, icon] of Object.entries(set.icons)) {
       const systemStyleObject = getIconCSSRules(icon) as SystemStyleObject;
 
-      recipes[camelizedPrefix + pascalize(name)] = {
-        base: systemStyleObject,
-      };
+      if (define.prefixAndName) {
+        recipes[camelizedPrefix + pascalize(name)] = {
+          base: systemStyleObject,
+        };
+      }
 
-      recipes[camelizedPrefix].variants!.name[name] = systemStyleObject;
+      if (define.prefix) {
+        recipes[camelizedPrefix].variants!.name[name] = systemStyleObject;
+      }
 
-      recipes.icon.variants!.name[set.prefix + ":" + name] = systemStyleObject;
+      if (define.icon) {
+        recipes.icon.variants!.name[set.prefix + ":" + name] =
+          systemStyleObject;
+      }
     }
   }
 
